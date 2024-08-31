fc64e086caafc91c7a17085f57d3764aec0c199f
miyemicodes <lizzymiyemi@gmail.com>
2024-08-30 20:42:57 -0700

    Category

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -3,12 +3,13 @@ import { FaCameraRetro } from "react-icons/fa6";
 
 export default function Addproduct() {
   return (
-    <div className="flex flex-col items-center justify-center gap-8">
-      <div className="flex items-center justify-center border p-5 rounded-full">
+    <div className="flex flex-col items-center justify-center gap-8 p-2">
+      <button className="flex flex-col items-center justify-center gap-2 border p-5 rounded-full">
         <FaCameraRetro className="text-6xl text-[#967f50]" />
-      </div>
+        <p className="text-sm text-[#967f50]">Upload Image</p>
+      </button>
 
-      <form className="flex flex-col items-center justify-center w-full px-4">
+      <form className="flex flex-col items-center justify-center gap-4 w-full px-4">
         <div className="flex flex-col items-start justify-start w-full">
           <label className="font-semibold">Item Name</label>
           <input
@@ -26,6 +27,24 @@ export default function Addproduct() {
             className="border placeholder:text-sm px-3 py-2 rounded-lg outline-none w-full"
           />
         </div>
+
+        <select className="border p-3 rounded-lg outline-none text-sm bg-[#f5f5f2] w-full">
+          <option>Product By Category</option>
+          <option>Men</option>
+          <option>Women</option>
+          <option>Necklaces</option>
+          <option>Earrings</option>
+          <option>Bracelets</option>
+          <option>Rings</option>
+        </select>
+
+        <select className="border p-3 rounded-lg outline-none text-sm bg-[#f5f5f2] w-full">
+          <option>Brand By Category</option>
+          <option>Versace</option>
+          <option>Pandora</option>
+          <option>Tiffany & Co.</option>
+          <option>Boucheron </option>
+        </select>
 
         <div className="flex flex-col items-start justify-start">
           <p className="p-2 font-semibold">Select size :</p>
@@ -80,6 +99,10 @@ export default function Addproduct() {
           </ul>
         </div>
       </form>
+
+      <button className="text-[#967f50] border-2 border-[#967f50]  font-semibold hover:bg-[#967f50] w-full p-2 rounded-md hover:text-white">
+        ADD PRODUCT
+      </button>
     </div>
   );
 }
